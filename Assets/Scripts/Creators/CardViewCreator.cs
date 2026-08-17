765287651421c04cf7c05360138c0cce375e5214
Stavros Kyriacou <stavroslkyriacou@gmail.com>
2026-08-17 11:52:35 +1000

    no longer handles card visuals on creation

--- Assets/Scripts/Creators/CardViewCreator.cs
+++ Assets/Scripts/Creators/CardViewCreator.cs
@@ -1,18 +1,13 @@
-using DG.Tweening;
 using UnityEngine;
 
 public class CardViewCreator : Singleton<CardViewCreator>
 {
     [SerializeField] private CardView _cardViewPrefab;
-    public int maxHandSize;
     [SerializeField] private float _scaleUpDuration;
 
     public CardView CreateCardView(Vector3 position, Quaternion rotation)
     {
         CardView cardView = Instantiate(_cardViewPrefab, transform.position, transform.rotation);
-        cardView.SetState(CardState.InDeck);
-        cardView.transform.localScale = Vector3.zero;
-        // cardView.transform.DOScale(Vector3.one, scaleUpDuration);
         return cardView;
     }
 }
