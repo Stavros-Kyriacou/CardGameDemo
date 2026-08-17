c8691d69d2eb7bfaaf9f283d5f83159225eff683
Stavros Kyriacou <stavroslkyriacou@gmail.com>
2026-08-17 17:42:26 +1000

    add targeting type and max targets

--- Assets/Scripts/Card Scripts/CardData.cs
+++ Assets/Scripts/Card Scripts/CardData.cs
@@ -7,10 +7,11 @@ public class CardData : ScriptableObject
     public string CardName;
     public string CardDescription;
     public int ManaCost;
+    public TargetingType TargetingType;
+    [Min(0)]
+    public int MaxTargets;
 
     [SerializeReference]
     [SubclassSelector]
     public List<CardEffect> effects = new();
-
-
 }
