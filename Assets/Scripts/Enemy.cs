6d37b16a16e4a5016195651e61f628b4bbb54152
Stavros Kyriacou <stavroslkyriacou@gmail.com>
2026-08-19 13:07:24 +1000

    enemy health test

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,9 +1,21 @@
-using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Enemy : MonoBehaviour, IPointerClickHandler
 {
+    private int _health = 10;
+    public void TakeDamage(int damage)
+    {
+        if (_health > 0)
+        {
+            _health -= damage;
+            Debug.Log("Took " + damage + " damage");
+        }
+        else
+        {
+            Debug.Log("I am dead");
+        }
+    }
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("clicked");
