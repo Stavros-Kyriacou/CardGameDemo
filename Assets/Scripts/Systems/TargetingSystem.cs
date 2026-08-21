13d1bb051367c581cee2205d470aae5d3b735b5b
Stavros Kyriacou <stavroslkyriacou@gmail.com>
2026-08-21 10:04:22 +1000

    add random enemy targeting type

--- Assets/Scripts/Systems/TargetingSystem.cs
+++ Assets/Scripts/Systems/TargetingSystem.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class TargetingSystem : Singleton<TargetingSystem>
 {
@@ -12,6 +13,7 @@ public class TargetingSystem : Singleton<TargetingSystem>
     public void BeginTargeting(CardView card)
     {
         var targets = new List<Enemy>();
+
         switch (card.Data.TargetingType)
         {
             case TargetingType.None:
@@ -27,12 +29,20 @@ public class TargetingSystem : Singleton<TargetingSystem>
                 TargetsSelected?.Invoke(_battleManager.Enemies);
                 break;
             case TargetingType.RandomEnemy:
-
+                targets = GetRandomEnemy();
+                TargetsSelected?.Invoke(targets);
                 break;
 
             default:
                 break;
         }
 
+    }
+    private List<Enemy> GetRandomEnemy()
+    {
+        var targets = new List<Enemy>();
+        var randomIndex = UnityEngine.Random.Range(0, _battleManager.Enemies.Count);
+        targets.Add(_battleManager.Enemies[randomIndex]);
+        return targets;
     }
 }
