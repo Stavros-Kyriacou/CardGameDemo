10d4644df5e7040e8a93572c038e8ba750c74c43
Stavros Kyriacou <stavroslkyriacou@gmail.com>
2026-08-22 16:13:07 +1000

    invoke Clicked event for targeting system. highlight when targetable

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,24 +1,51 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Enemy : MonoBehaviour, IPointerClickHandler
 {
-    private int _health = 10;
+    public bool IsTargetable;
+    public bool IsSelected { get; private set; }
+    [SerializeField] private int _health = 10;
+    [SerializeField] private TMP_Text _healthText;
+    [SerializeField] private SpriteRenderer _highlightedSprite;
+    [SerializeField] private SpriteRenderer _selectedSprite;
+
+    public event Action<Enemy> Clicked;
+
+    private void Start()
+    {
+        _healthText.text = "HP: " + _health;
+        SetHighlight(false);
+        SetSelected(false);
+        IsSelected = false;
+    }
     public void TakeDamage(int damage)
     {
         if (_health > 0)
         {
             _health -= damage;
-            Debug.Log("Took " + damage + " damage");
+            _healthText.text = "HP: " + _health;
         }
         else
         {
             Debug.Log("I am dead");
         }
     }
+
+    public void SetHighlight(bool highlighted)
+    {
+        _highlightedSprite.enabled = highlighted;
+    }
+    public void SetSelected(bool selected)
+    {
+        _selectedSprite.enabled = selected;
+    }
     public void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log("clicked");
-        Debug.Log(eventData);
-    }
+        if (!IsTargetable) return;
+
+        Clicked?.Invoke(this);
+    }  
 }
