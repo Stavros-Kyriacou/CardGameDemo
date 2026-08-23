29437b44daadc43e2e6f1fac19b19c7c9356d7ac
Stavros Kyriacou <stavroslkyriacou@gmail.com>
2026-08-23 17:36:54 +1000

    use separate logic for resolving manual targeting damage

--- Assets/Scripts/DamageEffect.cs
+++ Assets/Scripts/DamageEffect.cs
@@ -8,9 +8,19 @@ public class DamageEffect : CardEffect
 
     public override void Resolve(CardContext context)
     {
-        if (targetingIndex + 1 > context.Targets.Count)
-            return;
+        if (context.Card.Data.TargetingRules.TargetingType == TargetingType.Manual)
+        {
+            if (targetingIndex + 1 > context.Targets.Count)
+                return;
 
-        context.Targets[targetingIndex].TakeDamage(damage);
+            context.Targets[targetingIndex].TakeDamage(damage);
+        }
+        else
+        {
+            foreach (var target in context.Targets)
+            {   
+                target.TakeDamage(damage);
+            }
+        }
     }
 }
